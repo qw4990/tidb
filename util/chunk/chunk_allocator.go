5297cb9f250c5bf28ec2a06257f6342831a3ac0a
qw4990 <qw4990@163.com>
2019-05-10 21:01:41 +0800

    fix

--- util/chunk/chunk_allocator.go
+++ util/chunk/chunk_allocator.go
@@ -2,11 +2,9 @@ package chunk
 
 import (
 	"fmt"
-	"sync"
-	"unsafe"
-
 	"github.com/cznic/mathutil"
 	"github.com/pingcap/tidb/types"
+	"sync"
 )
 
 type BufAllocator struct {
@@ -100,8 +98,9 @@ func NewChunkWithAllocator(a *BufAllocator, fields []*types.FieldType, cap, maxC
 		if elemLen == varElemLen {
 			estimatedElemLen := 8
 			col := columnPool.Get().(*column)
-			offsets := a.Alloc(8, (cap+1)*8)
-			col.offsets = *(*[]int64)(unsafe.Pointer(&offsets))
+			//offsets := a.Alloc(8, (cap+1)*8)
+			//col.offsets = *(*[]int64)(unsafe.Pointer(&offsets))
+			col.offsets = make([]int64, 1, cap+1)
 			col.elemBuf = nil
 			col.data = a.Alloc(0, cap*estimatedElemLen)
 			col.nullBitmap = a.Alloc(0, cap>>3)
@@ -126,8 +125,9 @@ func ReleaseChunk(chk *Chunk) {
 	}
 	for _, c := range chk.columns {
 		if c.offsets != nil { // varElemLen
-			buf := *(*[]byte)(unsafe.Pointer(&c.offsets))
-			chk.a.Free(buf)
+			//buf := *(*[]byte)(unsafe.Pointer(&c.offsets))
+			//chk.a.Free(buf)
+			c.offsets = nil
 		} else {
 			chk.a.Free(c.elemBuf)
 		}
