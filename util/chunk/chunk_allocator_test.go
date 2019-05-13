83f17cdaf4ed874b7e56e83d36fa582fb832204e
qw4990 <qw4990@163.com>
2019-05-13 11:32:35 +0800

    add UT

---
+++ util/chunk/chunk_allocator_test.go
@@ -0,0 +1,39 @@
+package chunk
+
+import (
+	"runtime"
+	"sync/atomic"
+	"testing"
+)
+
+func BenchmarkAllocMultiBuf(b *testing.B) {
+	bufSize := []int{32, 64, 128, 256, 512, 1024, 4096}
+	n := len(bufSize)
+	m := NewMultiBufAllocator(8, 15, 32)
+	runtime.GC()
+	b.ResetTimer()
+	b.RunParallel(func(p *testing.PB) {
+		var i uint32
+		for p.Next() {
+			c := bufSize[int(atomic.AddUint32(&i, 1))%n]
+			buf := m.Alloc(0, c)
+			buf = append(buf, '0')
+			m.Free(buf)
+		}
+	})
+}
+
+func BenchmarkAllocGolangSlice(b *testing.B) {
+	bufSize := []int{32, 64, 128, 256, 512, 1024, 4096}
+	n := len(bufSize)
+	runtime.GC()
+	b.ResetTimer()
+	b.RunParallel(func(p *testing.PB) {
+		var i uint32
+		for p.Next() {
+			c := bufSize[int(atomic.AddUint32(&i, 1))%n]
+			buf := make([]byte, 0, c)
+			buf = append(buf, '0')
+		}
+	})
+}
