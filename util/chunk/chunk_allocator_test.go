7b1652b4513eb73b6cf4f362ac19274f97919412
qw4990 <qw4990@163.com>
2019-05-23 15:20:45 +0800

    remove some tests

--- util/chunk/chunk_allocator_test.go
+++ util/chunk/chunk_allocator_test.go
@@ -1,73 +1,14 @@
 package chunk
 
 import (
-	"github.com/pingcap/parser/charset"
-	"github.com/pingcap/parser/mysql"
-	"github.com/pingcap/tidb/types"
 	"runtime"
 	"sync/atomic"
 	"testing"
+
+	"github.com/pingcap/parser/charset"
+	"github.com/pingcap/parser/mysql"
+	"github.com/pingcap/tidb/types"
 )
-
-func BenchmarkAllocMultiBuf(b *testing.B) {
-	bufSize := []int{32, 64, 128, 256, 512, 1024, 4096}
-	n := len(bufSize)
-	m := NewMultiBufAllocator(8, 15, 32)
-	runtime.GC()
-	b.ResetTimer()
-	b.RunParallel(func(p *testing.PB) {
-		var i uint32
-		for p.Next() {
-			c := bufSize[int(atomic.AddUint32(&i, 1))%n]
-			buf := m.Alloc(0, c)
-			buf = append(buf, '0')
-			m.Free(buf)
-		}
-	})
-}
-
-func BenchmarkAllocGolangSlice(b *testing.B) {
-	bufSize := []int{32, 64, 128, 256, 512, 1024, 4096}
-	n := len(bufSize)
-	runtime.GC()
-	b.ResetTimer()
-	b.RunParallel(func(p *testing.PB) {
-		var i uint32
-		for p.Next() {
-			c := bufSize[int(atomic.AddUint32(&i, 1))%n]
-			buf := make([]byte, 0, c)
-			buf = append(buf, '0')
-		}
-	})
-}
-
-func BenchmarkAllocMultiBufSingle(b *testing.B) {
-	bufSize := []int{32, 64, 128, 256, 512, 1024, 4096}
-	n := len(bufSize)
-	m := NewMultiBufAllocator(8, 15, 32)
-	runtime.GC()
-	b.ResetTimer()
-	var k uint32
-	for i := 0; i < b.N; i++ {
-		c := bufSize[int(atomic.AddUint32(&k, 1))%n]
-		buf := m.Alloc(0, c)
-		buf = append(buf, '0')
-		m.Free(buf)
-	}
-}
-
-func BenchmarkAllocGolangSliceSingle(b *testing.B) {
-	bufSize := []int{32, 64, 128, 256, 512, 1024, 4096}
-	n := len(bufSize)
-	runtime.GC()
-	b.ResetTimer()
-	var k uint32
-	for i := 0; i < b.N; i++ {
-		c := bufSize[int(atomic.AddUint32(&k, 1))%n]
-		buf := make([]byte, 0, c)
-		buf = append(buf, '0')
-	}
-}
 
 func BenchmarkNewChunkWithAllocator(b *testing.B) {
 	bufSize := []int{32, 64, 128, 256, 512, 1024, 4096}
