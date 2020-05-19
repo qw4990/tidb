fa9c8a6fc9b251e2f4f9c3fc6cb3904bda396b6b
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-05-19 20:28:06 +0800

    fixup

--- executor/hashagg_table.go
+++ executor/hashagg_table.go
@@ -1,21 +1,6 @@
-// Copyright 2020 PingCAP, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package executor
 
 import (
-	"github.com/pingcap/tidb/sessionctx"
-	"github.com/pingcap/tidb/util/logutil"
 	"io/ioutil"
 	"path"
 	"sync"
@@ -25,6 +10,8 @@ import (
 	"github.com/pingcap/goleveldb/leveldb"
 	"github.com/pingcap/tidb/config"
 	"github.com/pingcap/tidb/executor/aggfuncs"
+	"github.com/pingcap/tidb/sessionctx"
+	"github.com/pingcap/tidb/util/logutil"
 	"github.com/pingcap/tidb/util/memory"
 )
 
