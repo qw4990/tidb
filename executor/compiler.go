b2c70171e20c0d003931dc896d21c1e99c5de328
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2021-12-20 17:48:34 +0800

    fixup

--- executor/compiler.go
+++ executor/compiler.go
@@ -47,6 +47,13 @@ type Compiler struct {
 	Ctx sessionctx.Context
 }
 
+func (c *Compiler) handleCERecord() {
+	fmt.Println(">>> record size >>> ", len(c.Ctx.GetSessionVars().StmtCtx.OptimizerCETrace))
+	for _, r := range c.Ctx.GetSessionVars().StmtCtx.OptimizerCETrace {
+		fmt.Println("-->>> ", r)
+	}
+}
+
 // Compile compiles an ast.StmtNode to a physical plan.
 func (c *Compiler) Compile(ctx context.Context, stmtNode ast.StmtNode) (*ExecStmt, error) {
 	if span := opentracing.SpanFromContext(ctx); span != nil && span.Tracer() != nil {
@@ -62,9 +69,21 @@ func (c *Compiler) Compile(ctx context.Context, stmtNode ast.StmtNode) (*ExecStm
 		return nil, err
 	}
 
+	if c.Ctx.GetSessionVars().CostCalibrationMode > 0 {
+		if c.Ctx.GetSessionVars().CostCalibrationMode == 1 {
+			c.Ctx.GetSessionVars().StmtCtx.EnableOptimizerCETrace = true
+		} else if c.Ctx.GetSessionVars().CostCalibrationMode == 2 {
+			c.Ctx.GetSessionVars().StmtCtx.EnableOptimizerCETrace = true
+			c.Ctx.GetSessionVars().StmtCtx.EnableUsingTrueCE = true
+		}
+	}
+
 	finalPlan, names, err := planner.Optimize(ctx, c.Ctx, stmtNode, ret.InfoSchema)
 	if err != nil {
 		return nil, err
+	}
+	if c.Ctx.GetSessionVars().StmtCtx.EnableOptimizerCETrace {
+		c.handleCERecord()
 	}
 
 	failpoint.Inject("assertStmtCtxIsStaleness", func(val failpoint.Value) {
