427641fddd539cd2c3177804855bf0eb07fdf6bd
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2021-12-21 22:10:37 +0800

    fixup

--- planner/core/logical_plans.go
+++ planner/core/logical_plans.go
@@ -771,7 +771,7 @@ func (ds *DataSource) deriveCommonHandleTablePathStats(path *util.AccessPath, co
 	}
 	// If the `CountAfterAccess` is less than `stats.RowCount`, there must be some inconsistent stats info.
 	// We prefer the `stats.RowCount` because it could use more stats info to calculate the selectivity.
-	if path.CountAfterAccess < ds.stats.RowCount && !isIm {
+	if path.CountAfterAccess < ds.stats.RowCount-1 && !isIm {
 		path.CountAfterAccess = math.Min(ds.stats.RowCount/SelectionFactor, float64(ds.statisticTable.Count))
 	}
 	return nil
@@ -849,7 +849,7 @@ func (ds *DataSource) deriveTablePathStats(path *util.AccessPath, conds []expres
 	path.CountAfterAccess, err = ds.statisticTable.GetRowCountByIntColumnRanges(ds.ctx, pkCol.ID, path.Ranges)
 	// If the `CountAfterAccess` is less than `stats.RowCount`, there must be some inconsistent stats info.
 	// We prefer the `stats.RowCount` because it could use more stats info to calculate the selectivity.
-	if path.CountAfterAccess < ds.stats.RowCount && !isIm {
+	if path.CountAfterAccess < ds.stats.RowCount-1 && !isIm {
 		path.CountAfterAccess = math.Min(ds.stats.RowCount/SelectionFactor, float64(ds.statisticTable.Count))
 	}
 	return err
