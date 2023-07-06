22916a227bdcbd27c5290c0a43e7d3f4e28e2e18
qw4990 <zhangyuanjia@pingcap.com>
2023-07-06 20:12:58 +0800

    fixup

--- statistics/handle/cache/internal/inner.go
+++ statistics/handle/cache/internal/inner.go
@@ -47,3 +47,75 @@ type StatsCacheInner interface {
 	// Front returns the front element's owner tableID, only used for test
 	Front() int64
 }
+
+// COWStatsCache is a copy-on-write stats cache.
+type COWStatsCache interface {
+	// GetByQuery retrieves cache triggered by a query. Usually used in LRU.
+	GetByQuery(int64) (*statistics.Table, bool)
+	// Get gets the cache.
+	Get(int64) (*statistics.Table, bool)
+	// Cost returns the memory usage of the cache.
+	Cost() int64
+	// Update creates a new COWStatsCache and updates on the new cache and returns it.
+	Update(tables []*statistics.Table, deletedIDs []int64, newVersion uint64, byQuery bool) COWStatsCache
+	// Version returns the version of the cache.
+	// initVersion is the version set when initializing this cache.
+	// updateCounter is the number of update operation on this cache.
+	Version() (initVersion, updateCounter uint64)
+}
+
+type cowStatsCache struct {
+	inner         StatsCacheInner
+	initVersion   uint64
+	updateCounter uint64
+}
+
+// NewCOWStatsCache creates a new COWStatsCache.
+func NewCOWStatsCache(c StatsCacheInner, version uint64) COWStatsCache {
+	return &cowStatsCache{
+		inner:         c,
+		initVersion:   version,
+		updateCounter: 0,
+	}
+}
+
+// GetByQuery implements the GetByQuery method.
+func (c *cowStatsCache) GetByQuery(tableID int64) (*statistics.Table, bool) {
+	return c.inner.GetByQuery(tableID)
+}
+
+// Get implements the Get method.
+func (c *cowStatsCache) Get(tableID int64) (*statistics.Table, bool) {
+	return c.inner.Get(tableID)
+}
+
+// Cost implements the Cost method.
+func (c *cowStatsCache) Cost() int64 {
+	return c.inner.Cost()
+}
+
+// Update implements the Update method.
+func (c *cowStatsCache) Update(tables []*statistics.Table, deletedIDs []int64, newVersion uint64, byQuery bool) COWStatsCache {
+	newCache := &cowStatsCache{
+		inner:         c.inner.Copy(),
+		initVersion:   c.initVersion,
+		updateCounter: c.updateCounter + 1,
+	}
+	for _, tbl := range tables {
+		id := tbl.PhysicalID
+		if byQuery {
+			newCache.inner.PutByQuery(id, tbl)
+		} else {
+			newCache.inner.Put(id, tbl)
+		}
+	}
+	for _, id := range deletedIDs {
+		newCache.inner.Del(id)
+	}
+	return newCache
+}
+
+// Version implements the Version method.
+func (c *cowStatsCache) Version() (initVersion, updateCounter uint64) {
+	return c.initVersion, c.updateCounter
+}
