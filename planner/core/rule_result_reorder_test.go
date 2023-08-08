9445585a1babea3d0912e76829d6a504faed7d7f
qw4990 <zhangyuanjia@pingcap.com>
2023-08-08 15:24:58 +0800

    fixup

--- planner/core/rule_result_reorder_test.go
+++ planner/core/rule_result_reorder_test.go
@@ -47,7 +47,7 @@ func TestUnknown1InWhereClause(t *testing.T) {
 func TestUnknown1InWhereClause2(t *testing.T) {
 	p := parser.New()
 	p.ParseSQL("select * from xw.t1 where 1=1 a.  ")
-	stmts, _, err := p.ParseSQL("select count(1) from t where a = 'Q13'")
+	stmts, _, err := p.ParseSQL("select count(1) from t where a = 1")
 	require.NoError(t, err)
 	var sb strings.Builder
 	restoreCtx := format.NewRestoreCtx(format.DefaultRestoreFlags, &sb)
