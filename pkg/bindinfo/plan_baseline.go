d3959b99e6e816dd4dd35af13d1b555d86ac28c1
qw4990 <zhangyuanjia@pingcap.com>
2023-12-08 11:48:37 +0800

    fixup

--- pkg/bindinfo/plan_baseline.go
+++ pkg/bindinfo/plan_baseline.go
@@ -3,21 +3,22 @@ package bindinfo
 import "time"
 
 type PlanBaseline struct {
-	Digest       string    // identifier of this plan baseline
-	SQLDigest    string    // identifier of the SQL statement
-	PlanDigest   string    // identifier of the execution plan
-	Creator      string    // the user who created this plan baseline
-	Source       string    // how the user create this plan baseline
-	Created      time.Time // when the user created this plan baseline
-	Modified     time.Time // the last modified time
-	LastActive   time.Time // the last time the plan baseline used
-	LastVerified time.Time // the last time the plan baseline verified
-	State        string    // the state of this plan baseline: accepted, preferred, unverified, disabled
+	Digest       string    // identifier of this plan baseline.
+	SQLDigest    string    // identifier of the SQL statement.
+	PlanDigest   string    // identifier of the execution plan.
+	Outline      string    // a set of hints corresponding to the SQL to generate the execution plan.
+	Creator      string    // the user who created this plan baseline.
+	Source       string    // how the user create this plan baseline.
+	Created      time.Time // when the user created this plan baseline.
+	Modified     time.Time // the last modified time.
+	LastActive   time.Time // the last time the plan baseline used.
+	LastVerified time.Time // the last time the plan baseline verified.
+	State        string    // the state of this plan baseline: accepted, preferred, unverified, disabled.
 	Comment      string    // the comment of this plan baseline
-	NormSQLText  string    // a normalized SQL statement, `select * from t where a<?`
-	SQLText      string    // a sample SQL statement will literals, `select * from t where a<10`
-	Plan         string    // the execution plan
-	Extras       string    // the extra information of this plan baseline
+	NormSQLText  string    // a normalized SQL statement, `select * from t where a<?`.
+	SQLText      string    // a sample SQL statement will literals, `select * from t where a<10`.
+	Plan         string    // the execution plan.
+	Extras       string    // the extra information of this plan baseline.
 }
 
 type PlanBaselineHandle interface {
