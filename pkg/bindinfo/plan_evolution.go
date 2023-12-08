d4ccd565c5d06d18a477f3352cc203cf4d090f9b
qw4990 <zhangyuanjia@pingcap.com>
2023-12-08 17:47:07 +0800

    fixup

--- pkg/bindinfo/plan_evolution.go
+++ pkg/bindinfo/plan_evolution.go
@@ -38,86 +38,92 @@ func (h *evolutionHandle) EvolveAll() error {
 }
 
 func (h *evolutionHandle) EvolveOne(sqlDigest string, autoAccept bool) (results []*EvolutionResult, err error) {
+	baselines, err := h.baselineHandle.GetBaseline("", sqlDigest, "", "")
+	if err != nil {
+		return nil, err
+	}
+
+	if results, err = h.evolveBaselines(baselines); err != nil {
+		return nil, err
+	}
+
+	if err := h.writeEvolutionResults(results); err != nil {
+		return nil, err
+	}
+
+	if autoAccept {
+		return results, h.autoAccept(baselines, results)
+	}
+	return
+}
+
+func (h *evolutionHandle) evolveBaselines(baselines []*PlanBaseline) (results []*EvolutionResult, err error) {
 	err = callWithSCtx(h.sPool, false, func(sctx sessionctx.Context) error {
-		baselines, err := h.baselineHandle.GetBaseline("", sqlDigest, "", "")
-		if err != nil {
-			return nil
-		}
-
-		results, err = evolveBaselines(sctx, baselines)
-		if err != nil {
-			return err
-		}
-
-		err = writeEvolutionResults(sctx, results)
-		if err != nil {
-			return err
-		}
-
-		if autoAccept {
-			var bestAcceptedExecTime time.Duration
-			for i := range baselines {
-				if baselines[i].Status != StateAccepted {
-					continue
-				}
-				if results[i].ExecTime < bestAcceptedExecTime {
-					bestAcceptedExecTime = results[i].ExecTime
-				}
+		for _, baseline := range baselines {
+			hintedStmt := hintedStmtFromBaseline(baseline)
+			rows, _, err := execRows(sctx, "explain format='verbose' "+hintedStmt) // TODO: what if it contains decorrelated sub-query?
+			if err != nil {
+				return err
 			}
+			estCost := rows[0].GetString(3)
 
-			for i := range baselines {
-				if baselines[i].Status != StateUnverified {
-					continue
-				}
-				if results[i].ExecTime < bestAcceptedExecTime {
-					if err = h.baselineHandle.UpdateBaselineStatus(baselines[i].Digest, baselines[i].SQLDigest, baselines[i].PlanDigest, StateAccepted); err != nil {
-						return err
-					}
-				}
+			execBegin := time.Now()
+			_, _, err = execRows(sctx, "explain analyze "+hintedStmt) // TODO: resource control
+			if err != nil {
+				return err
 			}
-		}
+			execTime := time.Since(execBegin)
 
+			results = append(results, &EvolutionResult{
+				BaselineDigest: baseline.Digest,
+				EstPlanCost:    estCost,
+				ExecTime:       execTime,
+				EvolutionTime:  time.Now(),
+				SQLText:        baseline.SQLText,
+				PlanText:       "", // TODO
+			})
+		}
 		return nil
 	})
 	return
 }
 
-func evolveBaselines(sctx sessionctx.Context, baselines []*PlanBaseline) (results []*EvolutionResult, err error) {
-	for _, baseline := range baselines {
-		hintedStmt := hintedStmtFromBaseline(baseline)
-		rows, _, err := execRows(sctx, "explain format='verbose' "+hintedStmt) // TODO: what if it contains decorrelated sub-query?
-		if err != nil {
-			return nil, err
+func (h *evolutionHandle) writeEvolutionResults(results []*EvolutionResult) error {
+	return callWithSCtx(h.sPool, true, func(sctx sessionctx.Context) error {
+		for _, r := range results {
+			if _, _, err := execRows(sctx, "insert into mysql.plan_evolution values (?,?,?,?,?,?)",
+				r.BaselineDigest, r.EstPlanCost, r.ExecTime, r.EvolutionTime, r.SQLText, r.PlanText); err != nil {
+				return err
+			}
 		}
-		estCost := rows[0].GetString(3)
-
-		execBegin := time.Now()
-		_, _, err = execRows(sctx, "explain analyze "+hintedStmt) // TODO: resource control
-		if err != nil {
-			return nil, err
-		}
-		execTime := time.Since(execBegin)
-
-		results = append(results, &EvolutionResult{
-			BaselineDigest: baseline.Digest,
-			EstPlanCost:    estCost,
-			ExecTime:       execTime,
-			EvolutionTime:  time.Now(),
-			SQLText:        baseline.SQLText,
-			PlanText:       "", // TODO
-		})
-	}
-	return
+		return nil
+	})
 }
 
-func writeEvolutionResults(sctx sessionctx.Context, results []*EvolutionResult) error {
-	for _, r := range results {
-		if _, _, err := execRows(sctx, "insert into mysql.plan_evolution values (?,?,?,?,?,?)",
-			r.BaselineDigest, r.EstPlanCost, r.ExecTime, r.EvolutionTime, r.SQLText, r.PlanText); err != nil {
-			return err
+func (h *evolutionHandle) autoAccept(baselines []*PlanBaseline, results []*EvolutionResult) error {
+	return callWithSCtx(h.sPool, true, func(sctx sessionctx.Context) error {
+		var bestAcceptedExecTime time.Duration
+		for i := range baselines {
+			if baselines[i].Status != StateAccepted {
+				continue
+			}
+			if results[i].ExecTime < bestAcceptedExecTime {
+				bestAcceptedExecTime = results[i].ExecTime
+			}
 		}
-	}
-	return nil
+
+		for i := range baselines {
+			if baselines[i].Status != StateUnverified {
+				continue
+			}
+			if results[i].ExecTime < bestAcceptedExecTime {
+				if err := h.baselineHandle.UpdateBaselineStatus(baselines[i].Digest, baselines[i].SQLDigest, baselines[i].PlanDigest, StateAccepted); err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	})
 }
 
 func hintedStmtFromBaseline(baseline *PlanBaseline) string {
