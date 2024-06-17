2b3ce97274f465aa8f13505e6823375ce2998719
qw4990 <zhangyuanjia@pingcap.com>
2024-06-17 18:09:22 +0800

    fixup

---
+++ pkg/planner/core/plan_cache_instance_lru_test.go
@@ -0,0 +1,26 @@
+// Package core Copyright 2024 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package core
+
+import (
+	"github.com/pingcap/tidb/pkg/util/size"
+	"testing"
+)
+
+func TestInstancePlanCache(t *testing.T) {
+	pc := NewInstancePlanCache(size.GB, size.GB)
+	sctx:= MockContext()
+	pc.Put(sctx, nil, nil, nil)
+}
