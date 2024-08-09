a8da1154a294d3c15dff76b2c45f40fe694f29a5
qw4990 <zhangyuanjia@pingcap.com>
2024-08-09 17:10:02 +0800

    fixup

--- pkg/planner/core/casetest/instanceplancache/main_test.go
+++ pkg/planner/core/casetest/instanceplancache/main_test.go
@@ -7,6 +7,15 @@ import (
 	"testing"
 )
 
+var (
+	typeInt      = "int"
+	typeVarchar  = "varchar(255)"
+	typeFloat    = "float"
+	typeDouble   = "double"
+	typeDecimal  = "decimal(10,2)"
+	typeDatetime = "datetime"
+)
+
 func randomItem(items ...string) string {
 	return items[rand.Intn(len(items))]
 }
@@ -20,26 +29,73 @@ func randomItems(items ...string) []string {
 	return res
 }
 
-func randomTables(n int) []string {
+func randomIntVal() string {
+	switch rand.Intn(4) {
+	case 0: // null
+		return "null"
+	case 1: // 0/positive/negative
+		return randomItem("0", "-1", "1", "100000000", "-1000000000")
+	case 2: // maxint32
+		return randomItem("2147483648", "2147483647", "2147483646", "-2147483648", "-2147483647", "-2147483646")
+	case 3: // maxint64
+		return randomItem("9223372036854775807", "9223372036854775808", "9223372036854775806", "-9223372036854775807", "-9223372036854775808", "-9223372036854775806")
+	default:
+		return randomItem(fmt.Sprintf("%v", rand.Intn(10)), fmt.Sprintf("-%v", rand.Intn(10)),
+			fmt.Sprintf("%v", rand.Intn(10)+1000000), fmt.Sprintf("-%v", rand.Intn(10)+1000000),
+			fmt.Sprintf("%v", rand.Intn(10)+100000000000), fmt.Sprintf("-%v", rand.Intn(10)+100000000000),
+			fmt.Sprintf("%v", rand.Intn(10)+1000000000000000), fmt.Sprintf("-%v", rand.Intn(10)+1000000000000000))
+	}
+}
+
+func prepareTableData(t string, rows int, colTypes []string) []string {
+	colValues := make([][]string, len(colTypes))
+	for i, colType := range colTypes {
+		colValues[i] = make([]string, 0, rows)
+		switch colType {
+		case typeInt:
+			for j := 0; j < rows; j++ {
+				colValues[i] = append(colValues[i], randomIntVal())
+			}
+		default:
+			panic("not implemented")
+		}
+	}
+	var inserts []string
+	for i := 0; i < rows; i++ {
+		vals := make([]string, 0, len(colTypes))
+		for j := range colTypes {
+			vals = append(vals, colValues[j][i])
+		}
+		inserts = append(inserts, fmt.Sprintf("insert ignore into %s values (%s);", t, strings.Join(vals, ", ")))
+	}
+	return inserts
+}
+
+func prepareTables(n int) []string {
 	nCols := 6
 	sqls := make([]string, 0, n)
 	for i := 0; i < n; i++ {
 		cols := make([]string, 0, nCols)
 		colNames := []string{"c0", "c1", "c2", "c3", "c4", "c5"}
+		var colTypes []string
 		for j := 0; j < nCols; j++ {
-			cols = append(cols, fmt.Sprintf("c%d %v", j, randomItem("int", "varchar(255)", "float", "double", "decimal(10,2)", "datetime")))
+			colType := randomItem(typeInt)
+			colTypes = append(colTypes, colType)
+			cols = append(cols, fmt.Sprintf("c%d %v", j, colType))
 		}
 		pkCols := randomItems(colNames...)
 		idx1 := randomItems(colNames...)
 		idx2 := randomItems(colNames...)
 		sqls = append(sqls, fmt.Sprintf("create table t%d (%s, primary key (%s), index idx1 (%s), index idx2 (%s));",
 			i, strings.Join(cols, ", "), strings.Join(pkCols, ", "), strings.Join(idx1, ", "), strings.Join(idx2, ", ")))
+
+		sqls = append(sqls, prepareTableData(fmt.Sprintf("t%d", i), 100, colTypes)...)
 	}
 	return sqls
 }
 
 func TestPlanStatsLoad(t *testing.T) {
-	for _, s := range randomTables(10) {
+	for _, s := range prepareTables(10) {
 		fmt.Println(s)
 	}
 }
@@ -53,9 +109,3 @@ type CaseSchema struct {
 
 type CaseSQL struct {
 }
-
-/*
-
-select * from {T} t where col=?;
-
-*/
