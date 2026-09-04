5b207b082b589843189bc6451caa23f998167f3e
xzhangxian1008 <xzhangxian@foxmail.com>
2026-09-04 13:35:05 +0800

    add diagnostic mode

--- cmd/tidb-server/main.go
+++ cmd/tidb-server/main.go
@@ -37,6 +37,7 @@ import (
 	"github.com/pingcap/tidb/pkg/bindinfo"
 	"github.com/pingcap/tidb/pkg/config"
 	"github.com/pingcap/tidb/pkg/config/deploymode"
+	"github.com/pingcap/tidb/pkg/config/diagnosticmode"
 	"github.com/pingcap/tidb/pkg/config/kerneltype"
 	"github.com/pingcap/tidb/pkg/ddl"
 	"github.com/pingcap/tidb/pkg/domain"
@@ -109,6 +110,7 @@ const (
 	nmConfig           = "config"
 	nmConfigCheck      = "config-check"
 	nmConfigStrict     = "config-strict"
+	nmDiagnosticMode   = "diagnostic-mode"
 	nmStore            = "store"
 	nmStorePath        = "path"
 	nmHost             = "host"
@@ -168,10 +170,11 @@ const (
 )
 
 var (
-	version      *bool
-	configPath   *string
-	configCheck  *bool
-	configStrict *bool
+	version        *bool
+	configPath     *string
+	configCheck    *bool
+	configStrict   *bool
+	diagnosticMode *bool
 
 	// Base
 	store            *string
@@ -244,6 +247,7 @@ func initFlagSet() *flag.FlagSet {
 	configPath = fset.String(nmConfig, "", "config file path")
 	configCheck = flagBoolean(fset, nmConfigCheck, false, "check config file validity and exit")
 	configStrict = flagBoolean(fset, nmConfigStrict, false, "enforce config file validity")
+	diagnosticMode = flagBoolean(fset, nmDiagnosticMode, false, "enable process-wide diagnostic mode")
 
 	// Base
 	store = fset.String(nmStore, string(config.StoreTypeUniStore), fmt.Sprintf("registered store name, %v", config.StoreTypeList()))
@@ -410,6 +414,7 @@ func main() {
 		}
 	}
 	config.InitializeConfig(*configPath, *configCheck, *configStrict, overrideConfig, fset)
+	terror.MustNil(diagnosticmode.Initialize(*diagnosticMode))
 	if kerneltype.IsNextGen() {
 		terror.MustNil(initDeployMode(config.GetGlobalConfig()))
 	}
